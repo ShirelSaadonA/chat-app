a509d5594ccb17f18cbf220eb2c2a7e26d0f10bf
Shirel Saadon <saadonshirel@gmail.com>
2025-07-01 09:36:22 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+const users = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.on('join', (user) => {
+    user.id = socket.id;
+    users.push(user);
+    io.emit('userList', users);
+  });
+
+  socket.on('message', (msg) => {
+    const user = users.find(u => u.id === socket.id);
+    if (user) {
+      io.emit('message', { nickname: user.nickname, message: msg });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    const index = users.findIndex(u => u.id === socket.id);
+    if (index !== -1) {
+      users.splice(index, 1);
+      io.emit('userList', users);
+    }
+  });
+});
+
+http.listen(3000, () => {
+  console.log('שרת הצ\'אט פועל על http://localhost:3000');
+});
